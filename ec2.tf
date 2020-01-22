2b4ed0f37a6458b005e54021b9bb2307749a46f7
stutin <simontutin@gmail.com>
2020-01-22 16:47:34 +0000

    initial

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-0d542ef84ec55d71c"
+  instance_type = "t2.micro"
+  key_name      = "test"
+}
